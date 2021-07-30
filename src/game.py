cedbea65824b18e1fd4952019f28a4b3a62848c9
Cyril CONTER <cconter@agilepartner.net>
2021-07-30 20:57:16 +0200

    feat: add default pygame screen

---
+++ src/game.py
@@ -0,0 +1,37 @@
+import pygame as pg
+import sys
+# from world import World
+
+
+class Game:
+
+    def __init__(self) -> None:
+        self.__running = False
+        pg.init()
+        pg.display.set_caption("Game Of Life")
+        pg.display.set_mode((400, 400))
+
+    def run(self) -> None:
+
+        timer = pg.time.Clock()
+
+        while True:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    self.exit()
+
+            pg.display.update()
+            timer.tick(60)
+
+    def exit() -> None:
+        pg.quit()
+        sys.exit()
+
+
+def main() -> None:
+    game = Game()
+    game.run()
+
+
+if __name__ == "__main__":
+    main()
