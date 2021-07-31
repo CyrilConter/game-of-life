bf70cf63212e36704725cb689ac94bbaab144b58
Cyril CONTER <cconter@agilepartner.net>
2021-07-31 07:33:34 +0200

    feat: visualization - add grid display/hide

--- src/game.py
+++ src/game.py
@@ -8,8 +8,8 @@ NB_COL = int(1920/CELL_SIZE)
 NB_ROW = int(1040/CELL_SIZE)
 
 BOARD_BACK_COLOR = 'black'
-BOARD_LINE_COLOR = 'black'
-ALIVE_CELL_COLOR = 'white'
+BOARD_LINE_COLOR = '#121212'
+ALIVE_CELL_COLOR = 'red'
 FONT_COLOR = 'red'
 
 
@@ -18,6 +18,7 @@ class Game:
 
     def __init__(self) -> None:
         self.__paused = False
+        self.__display_grid = True
         self.__cycle = 0
         self.__world = World(NB_ROW, NB_COL)
 
@@ -55,6 +56,8 @@ class Game:
                 self.exit()
             elif event.key == pg.K_PAUSE:
                 self.pause_pressed()
+            elif event.key == pg.K_g:
+                self.__display_grid = not self.__display_grid
             elif event.key == pg.K_r:
                 self.reset()
 
@@ -88,6 +91,13 @@ class Game:
 
         screen_rect = self.__screen.get_rect()
         world_rect = world_surf.get_rect(center=(screen_rect.center))
+
+        if self.__display_grid:
+            for x in range(self.__world.width+1):
+                pg.draw.line(world_surf, BOARD_LINE_COLOR, (x * CELL_SIZE, 0), (x * CELL_SIZE, world_rect.height))
+            for y in range(self.__world.height+1):
+                pg.draw.line(world_surf, BOARD_LINE_COLOR, (0, y * CELL_SIZE), (world_rect.width, y * CELL_SIZE))
+
         self.__screen.blit(world_surf, world_rect)
 
     def draw_information(self):
@@ -95,12 +105,13 @@ class Game:
         alive = self.__world.alive_cells_count
         fps = int(self.__clock.get_fps())
         text_y = 5
-
-        self.draw_text(f'Cells : {total}', (10, text_y))
+        grid_state = 'ON' if self.__display_grid else 'OFF'
+        self.draw_text(f'Cells: {total}', (10, text_y))
         self.draw_text(f'| Alive: {alive}', (100, text_y))
-        self.draw_text(f'| Dead : {total-alive}', (200, text_y))
-        self.draw_text(f'| FPS : {fps}', (300, text_y))
-        self.draw_text(f'| Cycle : {self.__cycle}', (600, text_y))
+        self.draw_text(f'| Dead: {total-alive}', (200, text_y))
+        self.draw_text(f'| FPS: {fps}', (300, text_y))
+        self.draw_text(f'| Display Grid: {grid_state}', (450, text_y))
+        self.draw_text(f'| Cycle: {self.__cycle}', (600, text_y))
 
     def draw_text(self, msg: str, pos: tuple):
         font = pg.font.SysFont('comicsans', 20)
@@ -112,7 +123,7 @@ class Game:
         sys.exit()
 
 
-def main() -> None:
+def main() -> None:   
     game = Game()
     game.run()
     sys.exit()
